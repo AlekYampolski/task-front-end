28cfebf41ca3c9bcbb231d4eeaf9f1da1e6138a2
Alek Yampolski <salekyam@gmail.com>
2018-02-21 10:42:08 +0200

    Init commit

---
+++ UIDom.js
@@ -0,0 +1,124 @@
+/* ===FUNCTIONS FOR CREATING ELEMENTS=== */
+
+/* If item has multiple items we have to wrap 
+these items into <div class="grouped fields"></div> */
+var createMultipleContent = function(){
+    var mult = document.createElement('div');
+    mult.classList.add('grouped','fields');
+    return mult;
+}
+
+// <div class="content"></div>
+var createContentWrapper = function(){
+    var divContentWrapper = document.createElement("div");
+    divContentWrapper.classList.add("content");
+
+    return divContentWrapper
+}
+
+// <div class="ui form"></div>
+var createUiForm = function(){
+    var divForm = document.createElement("div");
+    divForm.classList.add("ui", "form");
+
+    return divForm;
+}
+
+/* CREATE SUB-ITEM IN MENU  WITH STRUCTURE: 
+    <div class="item menuitem" id="mitem1101">
+        <div class="ui ">
+            <label>One-way ANOVA (use group variable)...</label>
+        </div>
+    </div>
+*/
+var createItemContent = function(labelContent, ID) {
+
+  var menuitem = document.createElement("div");
+  menuitem.classList.add("item", "menuitem");
+  if(ID){menuitem.id = ID};
+
+  var ui = document.createElement("div");
+  ui.classList.add("ui");
+
+  var labelText = document.createTextNode(labelContent);
+
+  var label = document.createElement("label");
+  label.appendChild(labelText);
+
+  menuitem
+  .appendChild(ui)
+  .appendChild(label);
+
+  return menuitem;
+};
+
+
+/* CREATE HEADER-TITLE FOR ITEM */
+var createItemHeader = function(labelHeader) {
+  //textcontent for div
+  var textNode = document.createTextNode(labelHeader);
+  
+  // i.dropdown.icon
+  var elIcon = document.createElement("i");
+  elIcon.classList.add("dropdown", "icon");
+
+  // a.active.title.header
+  var elAnch = document.createElement("a");
+  elAnch.classList.add("active", "title", "header");
+  
+  elAnch.appendChild(elIcon);
+  elAnch.appendChild(textNode);
+
+  return elAnch;
+};
+
+
+
+/* CREATE THE WHOLE ITEM WITH HEADER-TITLE AND IT'S CONTENT OF SUB-ITEMS */
+var createMenuItem = function(item) {
+  //div item
+  var elDiv = document.createElement("div");
+  elDiv.classList.add("item");
+
+  /* If you are changing dom that cause expensive reflow, you can avoid it by using documentFragment as it is managed in the memory. */
+  var fragment = document.createDocumentFragment();
+
+  var targetDOM = fragment;
+  
+  var newItem = targetDOM.appendChild(elDiv);
+    newItem.insertAdjacentElement('afterbegin', createItemHeader(item.label));
+
+    var forContent = newItem.insertAdjacentElement('beforeend', createContentWrapper()).appendChild(createUiForm());
+    
+    /* If we have multiple sub-items we have to use createMultipleContent()
+    for wrapping multiple items...
+    But any way  it will array. It consists of length
+     */
+    if(Array.isArray(item.items)){
+      //If we have only one item we have to avoid additional createMultipleContent()
+       if(item.items.length>1){
+           forContent = forContent.appendChild(createMultipleContent());
+       } 
+       item.items.forEach(subItem => {
+           if(subItem.visibility){
+              var addAttr; // for setting attributes
+               /* If "id" is absent and “isenabled”=false" it is sub-menu  */
+               if(!subItem.id &&  !subItem.isenabled ){
+                   addAttr = forContent.insertAdjacentElement('beforeend', createItemContent(subItem.label));
+                   addAttr.classList.add('disabled');
+                   return;
+               }
+               var ID = "mitem" + subItem.id;   
+               addAttr = forContent.insertAdjacentElement('beforeend', createItemContent(subItem.label, ID));
+               addAttr.setAttribute('data-cmd', subItem.id );
+               addAttr.setAttribute('visibility', subItem.visibility );
+               addAttr.setAttribute('isenabled', subItem.isenabled );
+               addAttr.setAttribute('label', "menuitem" );
+            } 
+       })
+    }
+    
+    document.getElementById("menuCmds").appendChild(fragment);
+
+};
+
---
+++ app.js
@@ -0,0 +1,45 @@
+$(".ui.accordion").accordion();
+/* I've copied code from https://jsfiddle.net/alexst/cpcozcd4/ 
+but there is some issue with console.log($(this).id);. 
+It returns undefined.
+
+There is a link to https://stackoverflow.com/questions/10578566/jquery-this-id-return-undefined
+with a similar problem.
+
+Answer:
+
+    $(this) and this aren't the same. The first represents a jQuery object
+    wrapped around your element. The second is just your element. 
+    The id property exists on the element, but not the jQuery object. 
+    As such, you have a few options:
+
+    1. Access the property on the element directly:
+
+        this.id
+
+    2. Access it from the jQuery object:
+
+        $(this).attr("id")
+
+    3. Pull the object out of jQuery:
+
+        $(this).get(0).id; // Or $(this)[0].id
+
+    4. Get the id from the event object:
+ */
+$(".ui.menu").on("click", ".menuitem", function() {
+  if ($(this).hasClass("item")) {
+    console.log($(this).id);
+    // or use data- attr via dataset
+    alert($(this).attr("id") + "\n" + $(this).attr("data-cmd"));
+  }
+});
+
+//Get json file. Need live-server for fetch
+fetch('menu_en.json')
+.then(response => response.json())
+.then(jsonResponse => {
+    jsonResponse.items.forEach(item => {
+        createMenuItem(item);
+    });
+});  
